62b9deb85886404a6bf35e0340070edf32cdb13e
hyeonjaez <jhjh0022@naver.com>
2023-09-04 17:24:51 +0900

    feat: 문자열 대 소문자 변환기 solution() 구현

--- src/main/java/org/example/StringConverter/StringConverter.java
+++ src/main/java/org/example/StringConverter/StringConverter.java
@@ -1,4 +1,21 @@
-package org.example;
+package org.example.StringConverter;
 
 public class StringConverter {
+
+    public static String solution(String str) {
+        StringBuilder sb = new StringBuilder();
+        char[] tokens = str.toCharArray();
+        for (char token : tokens) {
+            if (65 <= token && token <= 90) {
+                sb.append((char) (token + 32));
+            } else if (97 <= token && token <= 122) {
+                sb.append((char) (token - 32));
+            } else {
+                sb.append(token);
+            }
+
+        }
+        return sb.toString();
+    }
+
 }
