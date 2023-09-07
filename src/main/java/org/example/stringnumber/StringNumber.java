4f043a9c04978addc4d8947778a2b56197418660
hyeonjaez <jhjh0022@naver.com>
2023-09-07 10:40:16 +0900

    solution 작성

--- src/main/java/org/example/stringnumber/StringNumber.java
+++ src/main/java/org/example/stringnumber/StringNumber.java
@@ -4,30 +4,43 @@ public class StringNumber {
 
     private static double correctCount = 0;
     private static final String[][] TESTCASES = new String[][] {
-            { "abciwkabc", "abc", "2" },
-            { "aciwcosck", "c", "3" },
-            { "banana", "na", "2" },
-            { "alhorithmtest", "th", "1"}
+            {"abciwkabc", "abc", "2"},
+            {"aciwcosck", "c", "3"},
+            {"banana", "na", "2"},
+            {"alhorithmtest", "th", "1"}
     };
 
     public static void main(String[] args) {
-        for (int i = 0; i < TESTCASES.length; i++)
+        for (int i = 0; i < TESTCASES.length; i++) {
             System.out.println("Testcase " + i + " = " + test(TESTCASES[i][0], TESTCASES[i][1], TESTCASES[i][2]));
+        }
 
-        System.out.println("정답률 = " + (int)(correctCount / TESTCASES.length * 100) + "%");
+        System.out.println("정답률 = " + (int) (correctCount / TESTCASES.length * 100) + "%");
     }
 
     private static boolean test(String input, String input2, String answer) {
         boolean res = String.valueOf(solution(input, input2)).equals(answer);
-        if (res)
+        if (res) {
             correctCount++;
+        }
         return res;
     }
 
 
-
     private static int solution(String str, String specificStr) {
         int count = 0;
+
+        for (int i = 0; i < str.length() - specificStr.length() + 1; i++) {
+            String result = "";
+
+            for (int j = i; j < specificStr.length() + i; j++) {
+                result += String.valueOf(str.charAt(j));
+            }
+
+            if (specificStr.equals(result)) {
+                count++;
+            }
+        }
 
         return count;
     }
