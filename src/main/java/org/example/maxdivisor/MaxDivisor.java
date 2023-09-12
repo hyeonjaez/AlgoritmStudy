26fd1595fe757fc3831e746c300d5e59b3cd51fc
hyeonjaez <jhjh0022@naver.com>
2023-09-12 10:39:01 +0900

    최대 최대공약수 solution 풀이

--- src/main/java/org/example/maxdivisor/MaxDivisor.java
+++ src/main/java/org/example/maxdivisor/MaxDivisor.java
@@ -1,2 +1,28 @@
-package org.example.maxdivisor;public class MaxDivisor {
+package org.example.maxdivisor;
+
+public class MaxDivisor {
+
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40};
+        int max = 0;
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (max < solution(arr[i], arr[j])) {
+                    max = solution(arr[i], arr[j]);
+                }
+            }
+        }
+        System.out.println(max);
+    }
+
+    public static int solution(int num1, int num2) {
+        int max = 0;
+        for (int i = 1; i <= num1 && i <= num2; i++) {
+            if (num1 % i == 0 && num2 % i == 0) {
+                max = i;
+            }
+        }
+
+        return max;
+    }
 }
