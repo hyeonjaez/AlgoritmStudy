7f0450cda498bb223ebd7f506d74a6df59cfda81
Fiat_lux <50399586+hyeonjaez@users.noreply.github.com>
2024-04-05 14:25:03 +0900

    [Silver IV] Title: 수 찾기, Time: 1396 ms, Memory: 54140 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,43 +1,69 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int N = scanner.nextInt();
-        int[] A = new int[N];
+    static int N;
+    static int M;
+    static int[] array;
+    static int[] find;
 
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        array = new int[N];
+//        array = Arrays.stream(br.readLine().split(" "))
+//                .mapToInt(Integer::parseInt)
+//                .sorted()
+//                .toArray();
+//
+        st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            A[i] = scanner.nextInt();
+            array[i] = Integer.parseInt(st.nextToken());
         }
-        Arrays.sort(A);
+        Arrays.sort(array);
 
-        int M = scanner.nextInt();
+        st = new StringTokenizer(br.readLine());
+        M = Integer.parseInt(st.nextToken());
+        find = new int[M];
+//        find = Arrays.stream(br.readLine().split(" "))
+//                .mapToInt(Integer::parseInt)
+//                .toArray();
+        st = new StringTokenizer(br.readLine());
 
         for (int i = 0; i < M; i++) {
-            boolean find = false;
-            int target = scanner.nextInt();
-            int start = 0;
-            int end = A.length - 1;
+            find[i] = Integer.parseInt(st.nextToken());
+        }
 
-            while (start <= end) {
-                int midIndex = (start + end) / 2;
-                int midNum = A[midIndex];
 
-                if (midNum > target) {
-                    end = midIndex - 1;
-                } else if (midNum < target) {
-                    start = midIndex + 1;
-                } else {
-                    find = true;
-                    break;
-                }
-            }
-            if (find) {
+        for (int i = 0; i < M; i++) {
+            if (binarySearch(find[i], 0, array.length - 1)) {
                 System.out.println(1);
             } else {
                 System.out.println(0);
             }
         }
+
+
+    }
+
+
+    public static boolean binarySearch(int key, int start, int end) {
+        int mid;
+        while (start <= end) {
+            mid = (start + end) / 2;
+            if (key == array[mid]) {
+                return true;
+            } else if (key > array[mid]) {
+                start = mid + 1;
+            } else if (key < array[mid]) {
+                end = mid - 1;
+            }
+        }
+        return false;
     }
 }
