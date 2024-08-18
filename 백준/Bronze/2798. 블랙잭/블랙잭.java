afeb8396bfe13fc2ed9efee689e10d740ef50551
Fiat_lux <50399586+hyeonjaez@users.noreply.github.com>
2024-08-18 22:46:36 +0900

    [Bronze II] Title: 블랙잭, Time: 104 ms, Memory: 14248 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.java
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -1,43 +1,53 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
+    static int[] array;
+    static int n;
+    static int m;
+    static int minResult;
+    static int[] result;
 
-    public static void main(String[] args) throws IOException {
-        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int N = Integer.parseInt(st.nextToken());
-            int M = Integer.parseInt(st.nextToken());
-            int[] array = new int[N];
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-            st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
 
-            for (int i = 0; i < N; i++) {
-                array[i] = Integer.parseInt(st.nextToken());
-            }
+        st = new StringTokenizer(br.readLine());
+        array = new int[n];
 
-            int result = 0;
-
-            int min = Integer.MAX_VALUE;
-
-            for (int i = 0; i < N - 2; i++) {
-                for (int j = i + 1; j < N - 1; j++) {
-                    for (int k = j + 1; k < N; k++) {
-                        int sum = array[i] + array[j] + array[k];
-                        if(sum <= M){
-                            if(min > M - sum){
-                                result = sum;
-                                min = M- sum;
-                            }
-                        }
-                    }
-                }
-            }
-
-            System.out.println(result);
-
+        for (int i = 0; i < n; i++) {
+            array[i] = Integer.parseInt(st.nextToken());
         }
+        result = new int[3];
+
+        minResult = Integer.MAX_VALUE;
+        back(0, 0);
+        System.out.println(m - minResult);
     }
+
+    public static void back(int start, int depth) {
+        if (depth == 3) {
+            int sum = 0;
+            for (int data : result) {
+                sum += data;
+            }
+
+            if (sum > m) {
+                return;
+            }
+
+            minResult = Math.min(minResult, m - sum);
+            return;
+        }
+        for (int i = start; i < n; i++) {
+            result[depth] = array[i];
+            back(i + 1, depth + 1);
+        }
+
+    }
+
 }
