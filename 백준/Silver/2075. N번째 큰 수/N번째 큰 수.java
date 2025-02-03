ef3bc3aae3006699d33c531e31c5479ecc482445
Fiat_lux <50399586+hyeonjaez@users.noreply.github.com>
2025-02-03 20:04:05 +0900

    [Silver III] Title: N번째 큰 수, Time: 800 ms, Memory: 275740 KB -BaekjoonHub

---
+++ 백준/Silver/2075. N번째 큰 수/N번째 큰 수.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+        StringTokenizer st;
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                pq.offer(Integer.parseInt(st.nextToken()));
+            }
+        }
+        for (int i = 0; i < n - 1; i++) {
+            pq.poll();
+        }
+        System.out.println(pq.poll());
+
+
+    }
+
+}
