2db54a4e8b504e2778dd72fa2580f3e6a0105cf5
Fiat_lux <50399586+hyeonjaez@users.noreply.github.com>
2025-02-12 21:52:27 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 1140 ms, Memory: 74912 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -3,41 +3,44 @@ import java.util.*;
 
 public class Main {
     static List<Integer>[] map;
-    static int[] result;
     static boolean[] visited;
+    static int[] result;
 
-    public static void main(String[] args) throws IOException {
-        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
-            int n = Integer.parseInt(br.readLine());
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-            map = new List[n + 1];
-            visited = new boolean[n + 1];
-            for (int i = 1; i <= n; i++) {
-                map[i] = new ArrayList<>();
-            }
+        int n = Integer.parseInt(br.readLine());
+        map = new List[n + 1];
+        visited = new boolean[n + 1];
+        result = new int[n + 1];
 
-            result = new int[n + 1];
-            StringTokenizer st;
-            for (int i = 0; i < n - 1; i++) {
-                st = new StringTokenizer(br.readLine());
-                int a = Integer.parseInt(st.nextToken());
-                int b = Integer.parseInt(st.nextToken());
-                map[a].add(b);
-                map[b].add(a);
-            }
-
-            dfs(1);
-
-            for (int i = 2; i < n + 1; i++) {
-                System.out.println(result[i]);
-            }
+        for (int i = 0; i < n + 1; i++) {
+            map[i] = new ArrayList<>();
         }
+
+
+        for (int i = 0; i < n - 1; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            map[a].add(b);
+            map[b].add(a);
+        }
+        dfs(1);
+        for (int i = 2; i <= n; i++) {
+            System.out.println(result[i]);
+        }
+
+
     }
 
     public static void dfs(int n) {
         if (visited[n]) {
             return;
         }
+
         visited[n] = true;
 
         for (int i : map[n]) {
