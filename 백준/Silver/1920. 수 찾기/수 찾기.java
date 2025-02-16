fc7871bf17401ce912b3829fa53774402fe939bb
Fiat_lux <50399586+hyeonjaez@users.noreply.github.com>
2025-02-16 14:53:09 +0900

    [Silver IV] Title: 수 찾기, Time: 1084 ms, Memory: 52476 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,69 +1,59 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    static int N;
-    static int M;
+    static int n;
     static int[] array;
-    static int[] find;
+    static int[] target;
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(br.readLine());
 
-        N = Integer.parseInt(st.nextToken());
-        array = new int[N];
-//        array = Arrays.stream(br.readLine().split(" "))
-//                .mapToInt(Integer::parseInt)
-//                .sorted()
-//                .toArray();
-//
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
+        array = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
             array[i] = Integer.parseInt(st.nextToken());
         }
+
+        int m = Integer.parseInt(br.readLine());
+
+        target = new int[m];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < m; i++) {
+            target[i] = Integer.parseInt(st.nextToken());
+        }
+
         Arrays.sort(array);
 
-        st = new StringTokenizer(br.readLine());
-        M = Integer.parseInt(st.nextToken());
-        find = new int[M];
-//        find = Arrays.stream(br.readLine().split(" "))
-//                .mapToInt(Integer::parseInt)
-//                .toArray();
-        st = new StringTokenizer(br.readLine());
-
-        for (int i = 0; i < M; i++) {
-            find[i] = Integer.parseInt(st.nextToken());
+        for (int i : target) {
+            System.out.println(isFind(i));
         }
-
-
-        for (int i = 0; i < M; i++) {
-            if (binarySearch(find[i], 0, array.length - 1)) {
-                System.out.println(1);
-            } else {
-                System.out.println(0);
-            }
-        }
-
 
     }
 
+    public static int isFind(int target) {
+        int start = 0;
+        int end = array.length - 1;
 
-    public static boolean binarySearch(int key, int start, int end) {
-        int mid;
         while (start <= end) {
-            mid = (start + end) / 2;
-            if (key == array[mid]) {
-                return true;
-            } else if (key > array[mid]) {
-                start = mid + 1;
-            } else if (key < array[mid]) {
+            int mid = (start + end) / 2;
+
+            if (target == array[mid]) {
+                return 1;
+            }
+
+            if (target < array[mid]) {
                 end = mid - 1;
+            } else {
+                start = mid + 1;
             }
         }
-        return false;
+
+        return 0;
     }
+
+
 }
