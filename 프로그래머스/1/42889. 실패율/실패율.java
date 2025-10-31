06f1a6b4b15246f7a202173aec5962e1c1495335
Fiat_lux <50399586+hyeonjaez@users.noreply.github.com>
2025-10-31 16:26:08 +0900

    [level 1] Title: 실패율, Time: 4810.24 ms, Memory: 385 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.java
+++ 프로그래머스/1/42889. 실패율/실패율.java
@@ -1,49 +1,56 @@
 import java.util.*;
 class Solution {
     public int[] solution(int N, int[] stages) {
-        int[] answer = {};
-        int[] successCount = new int[N + 1];
-
-        int[] dojeonCount = new int[N + 1];
-        Node[] result = new Node[N];
-
-        for (int s : stages) {
-            for (int i = 1; i < s; i++) {
-                successCount[i]++;
-                dojeonCount[i]++;
-            }
-
-            if (s <= N) {
-                dojeonCount[s]++;
-            }
+        int[] answer = new int[N];
+        Map<Integer, Integer> dojeon = new HashMap<>();
+        Map<Integer , Integer> success = new HashMap<>();
+        for(int i = 1; i <= N; i++){
+            dojeon.put(i, 0);
+            success.put(i, 0);
         }
-
-        for (int i = 0; i < N; i++) {
-            if(dojeonCount[i + 1] == 0){
-                result[i] = new Node(i + 1, 0);
+        
+        for(int i : stages){
+            for(int j = 1; j < i; j++){
+                success.put(j, success.get(j) + 1);
+                dojeon.put(j, dojeon.get(j) + 1);
+            }
+            
+            if(1 <= i && i<=N){
+                dojeon.put(i, dojeon.get(i) + 1);
+            }
+            
+        }
+        
+        List<Node> nodeList = new ArrayList<>();
+        Node[] nodes = new Node[N];
+        for(int i = 1; i <= N; i++){
+            if(dojeon.get(i) == 0){
+                nodes[i-1] = new Node(i, 0);
                 continue;
             }
-
-            result[i] = new Node(i + 1, (double) dojeonCount[i + 1] / successCount[i + 1]);
+            
+            nodes[i-1] = new Node(i, (double) dojeon.get(i) / success.get(i));
         }
-
-        Arrays.sort(result);
-
-
-        return Arrays.stream(result).mapToInt(o -> o.index).toArray();
+        
+        Arrays.sort(nodes);
+        
+        
+        
+        return Arrays.stream(nodes).mapToInt(o -> o.number).toArray();
     }
+    
+    
 }
 
-class Node implements Comparable<Node> {
-    int index;
+class Node implements Comparable<Node>{
+    int number;
     double count;
-
-    Node(int index, double count) {
-        this.index = index;
+    Node(int number, double count){
+        this.number = number;
         this.count = count;
     }
-
-    public int compareTo(Node o) {
-        return  Double.compare(o.count, this.count);
+    
+    public int compareTo(Node o){
+        return Double.compare(o.count, this.count);
     }
 }
