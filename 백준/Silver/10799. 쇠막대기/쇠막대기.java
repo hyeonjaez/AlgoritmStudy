c61838ba6abefcea552bc5c29e3df4be97e53af2
Fiat_lux <50399586+hyeonjaez@users.noreply.github.com>
2026-01-21 11:52:59 +0900

    [Silver II] Title: 쇠막대기, Time: 152 ms, Memory: 16472 KB -BaekjoonHub

--- 백준/Silver/10799. 쇠막대기/쇠막대기.java
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.java
@@ -1,32 +1,36 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Stack;
+import java.util.*;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String str = br.readLine();
 
-        Stack<Character> stack = new Stack<>();
+        char[] charArray = str.toCharArray();
 
-        char before = '0';
+        Stack<Character> stack = new Stack<>();
         int count = 0;
-        for(char ch : str.toCharArray()){
-            if(ch == ')' ){
-                if(before == '('){
+        char before = 0;
+
+        for (char now : charArray) {
+            if (now == '(') {
+                stack.push(now);
+
+            } else {
+                if (before == '(') {
                     stack.pop();
-                    count+= stack.size();
-                }
-                else{
+                    count += stack.size();
+                } else {
                     stack.pop();
                     count++;
                 }
             }
-            else stack.push(ch);
-            before = ch;
+            before = now;
         }
         System.out.println(count);
     }
-
 }
+
