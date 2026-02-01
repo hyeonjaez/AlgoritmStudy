8dd88f3e518bace0eb9e25231b8a3e3560751dff
Fiat_lux <50399586+hyeonjaez@users.noreply.github.com>
2026-02-01 11:33:05 +0900

    [Silver I] Title: 효율적인 해킹, Time: 8380 ms, Memory: 149544 KB -BaekjoonHub

--- 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.java
+++ 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.java
@@ -1,75 +1,70 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-
-    static List<List<Integer>> list;
+    static List<Integer> answer;
+    static int max = Integer.MIN_VALUE;
     static boolean[] visited;
-    static int count;
-    static Map<Integer, Integer> map;
+    static List<Integer>[] array;
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        list = new ArrayList<>();
-        map = new HashMap<>();
 
-        for (int i = 0; i <= N; i++) {
-            list.add(new ArrayList<>());
+        int n = Integer.parseInt(st.nextToken());
+        int count = Integer.parseInt(st.nextToken());
+
+        array = new ArrayList[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            array[i] = new ArrayList<>();
         }
 
-        for (int i = 0; i < M; i++) {
+
+        for (int i = 0; i < count; i++) {
             st = new StringTokenizer(br.readLine());
-            int end = Integer.parseInt(st.nextToken());
-            int start = Integer.parseInt(st.nextToken());
-            list.get(start).add(end);
+
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            array[b].add(a);
         }
 
-        for (int i = 1; i <= N; i++) {
-            count = 0;
-            visited = new boolean[N + 1];
-            Queue<Integer> queue = new LinkedList<>();
-            queue.add(i);
-            visited[i] = true;
+        for (int i = 1; i < n + 1; i++) {
+            visited = new boolean[n + 1];
+            int now = dfs(i);
 
-            while (!queue.isEmpty()) {
-                int now = queue.poll();
-
-                for (int a : list.get(now)) {
-                    if (!visited[a]) {
-                        visited[a] = true;
-                        queue.add(a);
-                        count++;
-                    }
-                }
-            }
-            map.put(i, count);
-
-
-        }
-        StringBuilder sb = new StringBuilder();
-        int max = 0;
-        for (int i : map.values()) {
-            if (max <= i) {
-                max = i;
+            if (now > max) {
+                max = now;
+                answer = new ArrayList<>();
+                answer.add(i);
+            } else if (now == max) {
+                answer.add(i);
             }
         }
 
-        for (int i = 1; i <= N; i++) {
-            if (max == map.get(i)) {
-                sb.append(i).append(" ");
+        for (int i : answer) {
+            System.out.print(i + " ");
+        }
+
+
+    }
+
+    public static int dfs(int start) {
+        visited[start] = true;
+
+        int count = 1;
+
+        for (int i : array[start]) {
+            if (!visited[i]) {
+                count += dfs(i);
             }
         }
-        System.out.println(sb);
+
+        return count;
     }
 }
