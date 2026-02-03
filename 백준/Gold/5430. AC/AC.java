8f1edec5b6d732d44be6568d60a24011a1ceddf2
Fiat_lux <50399586+hyeonjaez@users.noreply.github.com>
2026-02-03 16:39:25 +0900

    [Gold V] Title: AC, Time: 680 ms, Memory: 82048 KB -BaekjoonHub

--- 백준/Gold/5430. AC/AC.java
+++ 백준/Gold/5430. AC/AC.java
@@ -1,82 +1,90 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Deque;
-import java.util.Objects;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
+    static Deque<String> queue;
+    static boolean isReverse;
 
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int testCount = Integer.parseInt(br.readLine());
 
-    public static void main(String[] args) throws IOException {
+        isReverse = false;
+        StringBuilder sb = new StringBuilder();
 
-        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
-            int testCount = Integer.parseInt(br.readLine());
-            StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < testCount; i++) {
-                String[] methods = br.readLine().split("");
-                int dataCount = Integer.parseInt(br.readLine());
+        for (int i = 0; i < testCount; i++) {
+            String command = br.readLine();
+            isReverse = false;
+            queue = new ArrayDeque<>();
+            int count = Integer.parseInt(br.readLine());
 
-                StringTokenizer st = new StringTokenizer(br.readLine(), "[],");
-//                Deque<Integer> queue = Arrays.stream(br.readLine()
-//                                .replace("[", "")
-//                                .replace("]", "").split(","))
-//                        .map(Integer::parseInt).collect(Collectors.toCollection(ArrayDeque::new));
-                Deque<Integer> queue = new ArrayDeque<>();
+            String[] split = br.readLine().replace("[", "").replace("]", "").split(",");
 
-                for (int j = 0; j < dataCount; j++) {
-                    queue.add(Integer.parseInt(st.nextToken()));
-                }
-                AC(methods, queue, sb);
+            if(count> 0){
+                Collections.addAll(queue, split);
             }
-            System.out.println(sb);
+
+
+
+            char[] charArray = command.toCharArray();
+            boolean isError = false;
+            for (char c : charArray) {
+
+                if (cm(c)) {
+                    isError = true;
+                    sb.append("error");
+                    break;
+                }
+            }
+
+            if (!isError) {
+                sb.append("[");
+
+                while (!queue.isEmpty()) {
+                    if (!isReverse) {
+                        sb.append(queue.pollFirst());
+                    } else {
+                        sb.append(queue.pollLast());
+                    }
+
+                    if (!queue.isEmpty()) {
+                        sb.append(",");
+                    }
+
+                }
+                sb.append("]");
+            }
+
+            if(i != testCount - 1){
+                sb.append("\n");
+            }
+
         }
+
+        System.out.println(sb);
+
     }
 
-    public static void AC(String[] methods, Deque<Integer> deque, StringBuilder sb) {
-
-        boolean isReverse = false;
-
-        for (String method : methods) {
-            if (Objects.equals("R", method)) {
+    public static boolean cm(char command) {
+        switch (command) {
+            case 'R':
                 isReverse = !isReverse;
-                continue;
-            }
+                break;
+            case 'D':
+                if (queue.isEmpty()) {
+                    return true;
+                }
 
-            if (isReverse) {
-                if (deque.pollLast() == null) {
-                    sb.append("error\n");
-                    return;
+                if (!isReverse) {
+                    queue.pollFirst();
+                } else {
+                    queue.pollLast();
                 }
-            } else {
-                if (deque.pollFirst() == null) {
-                    sb.append("error\n");
-                    return;
-                }
-            }
         }
-        makeResult(sb, deque, isReverse);
+
+        return false;
     }
 
-    public static void makeResult(StringBuilder sb, Deque<Integer> deque, boolean isReverse) {
-        sb.append("[");
 
-        if (!deque.isEmpty()) {
-            if (isReverse) {
-                sb.append(deque.pollLast());
-
-                while (!deque.isEmpty()) {
-                    sb.append(",").append(deque.pollLast());
-                }
-            } else {
-                sb.append(deque.pollFirst());
-
-                while (!deque.isEmpty()) {
-                    sb.append(',').append(deque.pollFirst());
-                }
-            }
-        }
-        sb.append("]").append("\n");
-    }
 }
