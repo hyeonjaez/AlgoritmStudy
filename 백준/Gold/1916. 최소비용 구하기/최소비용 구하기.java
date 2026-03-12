28a386dcdc1f47dfebf53cb42dec52d098860038
Fiat_lux <50399586+hyeonjaez@users.noreply.github.com>
2026-03-12 17:04:16 +0900

    [Gold V] Title: 최소비용 구하기, Time: 464 ms, Memory: 51116 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
@@ -1,70 +1,89 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-	public static List<Edge>[] edgeList;
-	public static int[] distance;
-	public static boolean[] visited;
+    static int n;
+    static int m;
+    static List<Node>[] map;
+    static int[] distance;
+    static boolean[] visited;
 
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int cityCount = Integer.parseInt(br.readLine());
-		int edgeCount = Integer.parseInt(br.readLine());
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		edgeList = new List[cityCount + 1];
-		visited = new boolean[cityCount + 1];
-		distance = new int[cityCount + 1];
+        n = Integer.parseInt(br.readLine());
+        m = Integer.parseInt(br.readLine());
 
-		for (int i = 0; i < cityCount + 1; i++) {
-			edgeList[i] = new ArrayList<>();
-			distance[i] = Integer.MAX_VALUE;
-		}
-		StringTokenizer st;
-		for (int i = 0; i < edgeCount; i++) {
-			st = new StringTokenizer(br.readLine());
-			int number = Integer.parseInt(st.nextToken());
-			edgeList[number].add(new Edge(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
-		}
 
-		PriorityQueue<Edge> queue = new PriorityQueue<>();
-		st = new StringTokenizer(br.readLine());
+        map = new List[n + 1];
 
-		int start = Integer.parseInt(st.nextToken());
-		int end = Integer.parseInt(st.nextToken());
+        for (int i = 1; i <= n; i++) {
+            map[i] = new ArrayList<>();
+        }
 
-		queue.add(new Edge(start, 0));
-		distance[start] = 0;
+        distance = new int[n + 1];
+        Arrays.fill(distance, Integer.MAX_VALUE);
+        visited = new boolean[n + 1];
 
-		while (!queue.isEmpty()) {
-			Edge now = queue.poll();
-			if (visited[now.number]) {
-				continue;
-			}
-			visited[now.number] = true;
+        StringTokenizer st;
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
 
-			for (Edge next : edgeList[now.number]) {
-				if (distance[next.number] > distance[now.number] + next.power) {
-					distance[next.number] = distance[now.number] + next.power;
-					queue.add(new Edge(next.number, distance[next.number]));
-				}
-			}
-		}
-		System.out.println(distance[end]);
-	}
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            int count = Integer.parseInt(st.nextToken());
+
+            map[start].add(new Node(end, count));
+        }
+
+        st = new StringTokenizer(br.readLine());
+        int start = Integer.parseInt(st.nextToken());
+        int end = Integer.parseInt(st.nextToken());
+
+        distance[start] = 0;
+        da(start);
+
+        System.out.println(distance[end]);
+    }
+
+    public static void da(int start) {
+        PriorityQueue<Node> queue = new PriorityQueue<>();
+
+        queue.add(new Node(start, 0));
+
+        while (!queue.isEmpty()) {
+            Node now = queue.poll();
+
+            if (visited[now.end]) {
+                continue;
+            }
+
+            visited[now.end] = true;
+
+            for (Node next : map[now.end]) {
+                if (distance[next.end] > distance[now.end] + next.count) {
+                    distance[next.end] = distance[now.end] + next.count;
+
+                    queue.add(new Node(next.end, distance[next.end]));
+                }
+            }
+        }
+    }
+
+
 }
 
-class Edge implements Comparable<Edge> {
-	int number;
-	int power;
+class Node implements Comparable<Node> {
+    int end;
+    int count;
 
-	Edge(int number, int power) {
-		this.number = number;
-		this.power = power;
-	}
+    Node(int end, int count) {
+        this.end = end;
+        this.count = count;
+    }
 
-	public int compareTo(Edge o) {
-
-		return this.power - o.power;
-	}
-
+    public int compareTo(Node o) {
+        return this.count - o.count;
+    }
 }
