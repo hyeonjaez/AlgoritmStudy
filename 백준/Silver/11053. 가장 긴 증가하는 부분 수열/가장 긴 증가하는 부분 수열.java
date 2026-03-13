bf766297039654a663fe647cffbfa1e0ea224058
Fiat_lux <50399586+hyeonjaez@users.noreply.github.com>
2026-03-13 12:28:14 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 120 ms, Memory: 14444 KB -BaekjoonHub

---
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] array = new int[n];
+        int[] dp = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            array[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int answer = 0;
+
+        for (int i = 0; i < n; i++) {
+            dp[i] = 1;
+
+            for (int j = 0; j < i; j++) {
+                if (array[j] < array[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+
+            answer = Math.max(answer, dp[i]);
+        }
+
+        System.out.println(answer);
+
+
+    }
+
+
+}
