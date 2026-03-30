fbf53ab0d31530909ecba9f881b9e549227affbe
Fiat_lux <50399586+hyeonjaez@users.noreply.github.com>
2026-03-30 22:13:59 +0900

    [Gold I] Title: 구간 합 구하기, Time: 608 ms, Memory: 94676 KB -BaekjoonHub

--- 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.java
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.java
@@ -4,6 +4,7 @@ import java.util.*;
 
 public class Main {
     static long[] array;
+    static long[] tree;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -11,49 +12,81 @@ public class Main {
 
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
-        int k = Integer.parseInt(st.nextToken());
+        int j = Integer.parseInt(st.nextToken());
 
-        array = new long[n];
+        int treeHeight = 0;
+        int lenght = n;
 
-        for (int i = 0; i < n; i++) {
-            long now = Long.parseLong(br.readLine());
-
-            if (i >= 1) {
-                array[i] = array[i - 1] + now;
-            } else {
-                array[i] = now;
-            }
+        while (lenght != 0) {
+            lenght /= 2;
+            treeHeight++;
         }
 
-        for (int i = 0; i < m + k; i++) {
+        int treeSize = (int) Math.pow(2, treeHeight + 1);
+        int leftNodeStartIndex = treeSize / 2 - 1;
+        tree = new long[treeSize + 1];
+
+        for (int i = leftNodeStartIndex + 1; i <= leftNodeStartIndex + n; i++) {
+            tree[i] = Long.parseLong(br.readLine());
+        }
+
+        setTree(treeSize - 1);
+
+        for (int i = 0; i < m + j; i++) {
             st = new StringTokenizer(br.readLine());
+            long a = Integer.parseInt(st.nextToken());
+            int s = Integer.parseInt(st.nextToken());
+            long e = Long.parseLong(st.nextToken());
 
-            int command = Integer.parseInt(st.nextToken());
-            int target = Integer.parseInt(st.nextToken());
-            long value = Long.parseLong(st.nextToken());
-
-            if (command == 1) {
-                long be;
-                if (target == 1) {
-                    be = array[target - 1];
-                } else {
-                    be = array[target - 1] - array[target - 2];
-                }
-
-                for (int j = target - 1; j < n; j++) {
-                    array[j] = array[j] - be + value;
-                }
-
+            if (a == 1) {
+                change(leftNodeStartIndex + s, e);
             } else {
-                int max = Math.max(target, (int) value) - 1;
-                int min = Math.min(target, (int) value) - 1;
-
-                if (min != 0) {
-                    System.out.println(array[max] - array[min - 1]);
-                } else {
-                    System.out.println(array[max]);
-                }
+                s = s + leftNodeStartIndex;
+                e = e + leftNodeStartIndex;
+                System.out.println(get(s, (int) e));
             }
+
+        }
+
+
+    }
+
+    public static long get(int start, int end) {
+        long answer = 0;
+        while (start <= end) {
+            if (start % 2 == 1) {
+                answer += tree[start];
+                start++;
+            }
+            if (end % 2 == 0) {
+                answer += tree[end];
+                end--;
+            }
+
+            start /= 2;
+            end /= 2;
+        }
+
+        return answer;
+    }
+
+
+    public static void change(int index, long value) {
+        long diff = value - tree[index];
+
+        while (index > 0) {
+            tree[index] = tree[index] + diff;
+
+            index /= 2;
         }
     }
+
+    public static void setTree(int i) {
+        while (i != 1) {
+            tree[i / 2] += tree[i];
+            i--;
+        }
+    }
+
+
 }
