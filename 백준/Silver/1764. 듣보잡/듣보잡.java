3a2b1f1f27114d6528194784c20ae40dd0f1a9de
Fiat_lux <50399586+hyeonjaez@users.noreply.github.com>
2026-04-20 15:26:32 +0900

    [Silver IV] Title: 듣보잡, Time: 248 ms, Memory: 26268 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.java
+++ 백준/Silver/1764. 듣보잡/듣보잡.java
@@ -1,35 +1,41 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
 
-    public static void main(String[] args) {
-        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int n = Integer.parseInt(st.nextToken());
-            int m = Integer.parseInt(st.nextToken());
-            Set<String> set = new HashSet<>();
-            for (int i = 0; i < n; i++) {
-                set.add(br.readLine());
-            }
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-            List<String> list = new ArrayList<>();
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
 
-            for (int i = 0; i < m; i++) {
-                String target = br.readLine();
-                if (set.contains(target)) {
-                    list.add(target);
-                }
-            }
-            Collections.sort(list);
-            System.out.println(list.size());
-            for (String str : list) {
-                System.out.println(str);
-            }
+        Set<String> set = new HashSet<>();
 
-        } catch (Exception e) {
-            throw new RuntimeException(e);
+        for (int i = 0; i < n; i++) {
+            set.add(br.readLine());
         }
 
+        StringBuilder sb = new StringBuilder();
+        List<String> list = new ArrayList<>();
+        for (int i = 0; i < m; i++) {
+            String now = br.readLine();
+            if (set.contains(now)) {
+                list.add(now);
+            }
+        }
+
+        Collections.sort(list);
+
+        System.out.println(list.size());
+        list.forEach(o -> sb.append(o).append("\n"));
+
+        System.out.println(sb);
+
+
     }
+
+
 }
+
