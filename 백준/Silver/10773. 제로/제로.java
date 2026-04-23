e0aefe090714898878db912292d973a65848304c
Fiat_lux <50399586+hyeonjaez@users.noreply.github.com>
2026-04-23 21:32:04 +0900

    [Silver IV] Title: 제로, Time: 212 ms, Memory: 23928 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.java
+++ 백준/Silver/10773. 제로/제로.java
@@ -1,27 +1,39 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Stack;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
+
+        int testCount = Integer.parseInt(br.readLine());
+
+
         Stack<Integer> stack = new Stack<>();
-        for (int i = 0; i < N; i++) {
-            int num = Integer.parseInt(br.readLine());
-            if (num != 0) {
-                stack.push(num);
+
+        for (int i = 0; i < testCount; i++) {
+            int now = Integer.parseInt(br.readLine());
+
+            if (now == 0) {
+                if (!stack.isEmpty()) {
+                    stack.pop();
+                }
             } else {
-                stack.pop();
+                stack.push(now);
             }
         }
+
         int result = 0;
         while (!stack.isEmpty()) {
             result += stack.pop();
         }
 
         System.out.println(result);
+
+
     }
 
+
 }
+
