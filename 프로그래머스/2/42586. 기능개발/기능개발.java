6b3eed383d6a57e6fa0d2a6b4410f111815d3f4f
Fiat_lux <50399586+hyeonjaez@users.noreply.github.com>
2026-05-16 17:05:36 +0900

    [level 2] Title: 기능개발, Time: 4.14 ms, Memory: 61.4 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,6 +1,56 @@
+import java.util.*;
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        int[] answer = {};
-        return answer;
+        List<Integer> answer = new ArrayList<>();
+        Queue<Node> queue = new LinkedList<>();
+
+        Queue<Integer> remain = new LinkedList<>();
+
+        for (int i = 0; i < progresses.length; i++) {
+            queue.add(new Node(progresses[i], speeds[i]));
+        }
+
+        while (!queue.isEmpty()) {
+            Node now = queue.poll();
+            int nowResult = get(now.pro, now.sped);
+            int count = 1;
+            while (!queue.isEmpty()) {
+                Node next = queue.peek();
+                int nextResult = get(next.pro, next.sped);
+
+                if (nowResult >= nextResult) {
+                    queue.poll();
+                    count++;
+                } else {
+                    break;
+                }
+            }
+
+            answer.add(count);
+
+        }
+
+
+        return answer.stream().mapToInt(value -> value).toArray();
+    }
+
+    public int get(int pro, int sped) {
+        if ((100 - pro) % sped == 0) {
+            return (100 - pro) / sped;
+        } else {
+            return (100 - pro) / sped + 1;
+        }
+    }
+
+
+}
+
+class Node {
+    int pro;
+    int sped;
+
+    Node(int pro, int sped) {
+        this.pro = pro;
+        this.sped = sped;
     }
 }
