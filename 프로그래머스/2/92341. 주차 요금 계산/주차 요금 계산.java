0e1dddf541f19d809352609659ececdb06c855a1
Fiat_lux <50399586+hyeonjaez@users.noreply.github.com>
2026-05-19 13:36:56 +0900

    [level 2] Title: 주차 요금 계산, Time: 6.28 ms, Memory: 61.8 MB -BaekjoonHub

--- 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.java
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.java
@@ -1,55 +1,58 @@
 import java.util.*;
 class Solution {
     public int[] solution(int[] fees, String[] records) {
-        Map<Integer, Integer> map = new HashMap<>();
-        Map<Integer, Integer> totalTimeMap = new HashMap<>();
+        Map<String, Integer> map = new HashMap<>();
+        Map<String, Integer> totalMap = new HashMap<>();
 
         for (String record : records) {
             String[] split = record.split(" ");
-            String time = split[0];
-            int number = Integer.parseInt(split[1]);
-            String z = split[2];
 
-            if (z.equals("IN")) {
-                map.put(number, getCal(time));
+            if (split[2].equals("IN")) {
+                map.put(split[1], getTime(split[0]));
             } else {
-                if (map.containsKey(number)) {
-                    int inTime = map.remove(number);
-                    int total = getCal(time) - inTime;
-                    totalTimeMap.put(number, totalTimeMap.getOrDefault(number, 0) + total);
-                }
+                int inTime = map.get(split[1]);
+                int outTime = getTime(split[0]);
+
+                totalMap.put(split[1], totalMap.getOrDefault(split[1], 0) + outTime - inTime);
+
+                map.remove(split[1]);
             }
         }
-        String defaultTime = "23:59";
-        int endOfDay = getCal(defaultTime);
-        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            int carNumber = entry.getKey();
-            int inTime = entry.getValue();
-            int total = endOfDay - inTime;
 
-            totalTimeMap.put(carNumber, totalTimeMap.getOrDefault(carNumber, 0) + total);
+        int end = totalMinute(23, 59);
+        for (String car : map.keySet()) {
+            int in = map.get(car);
+            totalMap.put(car, totalMap.getOrDefault(car, 0) + end - in);
         }
 
-        return totalTimeMap.entrySet().stream().sorted(Map.Entry.comparingByKey()).mapToInt(entry -> money(fees, entry.getValue())).toArray();
+        List<String> cars = new ArrayList<>(totalMap.keySet());
+        Collections.sort(cars);
 
+        int[] answer = new int[cars.size()];
+
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = calFee(fees, totalMap.get(cars.get(i)));
+        }
+
+        return answer;
     }
 
-    public int money(int[] fees, int time) {
-        int defaultTime = fees[0];
-        int defaultFee = fees[1];
-        int minute = fees[2];
-        int fee = fees[3];
+    public int getTime(String target) {
+        String[] split = target.trim().split(":");
+        return totalMinute(Integer.parseInt(split[0]), Integer.parseInt(split[1]));
+    }
 
-        if (time <= defaultTime) {
+
+    public int calFee(int[] fees, int minute) {
+        if (minute <= fees[0]) {
             return fees[1];
         }
 
-
-        return defaultFee + (int) Math.ceil((double) (time - defaultTime) / minute) * fee;
+        int extra = minute - fees[0];
+        return fees[1] + (int) Math.ceil((double) extra / fees[2]) * fees[3];
     }
 
-    public int getCal(String time) {
-        String[] split = time.split(":");
-        return Integer.parseInt(split[0]) * 60 + Integer.parseInt(split[1]);
+    public int totalMinute(int hour, int minute) {
+        return hour * 60 + minute;
     }
 }
