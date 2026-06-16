31503ad0507df49f0df7c91ad4939d926628dbd7
Fiat_lux <50399586+hyeonjaez@users.noreply.github.com>
2026-06-16 23:02:35 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 4.89 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.java
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.java
@@ -1,71 +1,59 @@
 import java.util.*;
 class Solution {
     public int solution(String str1, String str2) {
-        int answer = 0;
-        List<String> a = split(converter(str1));
-        List<String> b = split(converter(str2));
-        
-        
-        if(a.isEmpty() && b.isEmpty()){
+        List<String> a = converter(str1);
+        List<String> b = converter(str2);
+
+
+        if (a.isEmpty() && b.isEmpty()) {
             return 65536;
         }
-        
-        Map<String, Integer> ca = countMap(a);
-        Map<String, Integer> cb = countMap(b);
-        
+
+        Map<String, Integer> ca = toMap(a);
+        Map<String, Integer> cb = toMap(b);
+
         int inter = 0;
         int union = 0;
-        
+
         Set<String> keys = new HashSet<>();
-        
+
         keys.addAll(ca.keySet());
         keys.addAll(cb.keySet());
-        
-        for(String k : keys){
+
+        for (String k : keys) {
             int va = ca.getOrDefault(k, 0);
             int vb = cb.getOrDefault(k, 0);
-            
+
             inter += Math.min(va, vb);
             union += Math.max(va, vb);
         }
-        
-        
+
         return (int) Math.floor((inter * 65536.0) / union);
-        
     }
-    
-    public String converter(String target){
-        StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < target.length(); i++){
-            char ch = target.charAt(i);
-            if(('a' <= ch && ch <= 'z') || ('A' <= ch && ch <= 'Z')){
-                sb.append(ch);
-            }else{
-                sb.append(" ");
-            }
-        }
-        return sb.toString();
-        
-    }
-    
-    public List<String> split(String target){
+
+    public List<String> converter(String str) {
         List<String> list = new ArrayList<>();
-        for(int i = 0; i < target.length() - 1; i++) {
-            if(target.charAt(i) != ' ' && target.charAt(i + 1) != ' '){
-                list.add(new StringBuilder().append(target.charAt(i)). append(target.charAt(i + 1)).toString().toLowerCase());
+        String target = str.toUpperCase();
+        for (int i = 0; i < target.length() - 1; i++) {
+            char now = target.charAt(i);
+            char next = target.charAt(i + 1);
+
+            if ('A' <= now && now <= 'Z') {
+                if ('A' <= next && next <= 'Z') {
+                    list.add(String.valueOf(now) + next);
+                }
             }
         }
+
         return list;
     }
-    
-    public Map<String, Integer> countMap(List<String> list){
+
+    public Map<String, Integer> toMap(List<String> list) {
         Map<String, Integer> map = new HashMap<>();
-        
-        for(String str : list){
+
+        for (String str : list) {
             map.put(str, map.getOrDefault(str, 0) + 1);
         }
         return map;
     }
-    
-    
 }
